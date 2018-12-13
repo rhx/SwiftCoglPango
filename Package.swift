16ce963bfb67dce717a02319e4572dd2f7c442dc
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 15:24:25 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,11 +1,20 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "CoglPango",
-    dependencies: [
-        .Package(url: "https://github.com/rhx/CCoglPango.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftPango.git", majorVersion: 2),
-        .Package(url: "https://github.com/rhx/SwiftCogl.git", majorVersion: 2),
+    products: [
+        .library(name: "CoglPango", targets: ["CoglPango"]),
     ],
-    swiftLanguageVersions: [3, 4]
+    dependencies: [
+        .package(url: "https://github.com/rhx/CPango.git", .branch("master")),
+        .package(url: "https://github.com/rhx/CCoglPango.git", .branch("master")),
+        .package(url: "https://github.com/rhx/SwiftPango.git", .branch("master")),
+        .package(url: "https://github.com/rhx/SwiftCogl.git", .branch("master")),
+    ],
+    targets: [
+        .target(name: "CoglPango", dependencies: ["Cogl", "Pango"]),
+        .testTarget(name: "CoglPangoTests", dependencies: ["CoglPango"]),
+    ]
 )
