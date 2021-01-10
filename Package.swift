406afac700353b75c88e27300bb226008718ef9d
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-10 10:20:31 +1000

    Adapt to updated directory structure

--- Package.swift
+++ Package.swift
@@ -6,11 +6,12 @@ let package = Package(
     name: "CoglPango",
     products: [ .library(name: "CoglPango", targets: ["CoglPango"]) ],
     dependencies: [
-        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("main")),
-        .package(name: "Cogl", url: "https://github.com/rhx/SwiftCogl.git", .branch("main")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
+        .package(name: "Pango", url: "https://github.com/rhx/SwiftPango.git", .branch("development")),
+        .package(name: "Cogl", url: "https://github.com/rhx/SwiftCogl.git", .branch("development")),
     ],
     targets: [
-	.systemLibrary(name: "CCoglPango", pkgConfig: "cogl-pango cogl-1.0 cogl-gl-1.0 cogl-path-1.0 pango pangoft2 cairo glib-2.0 gio-unix-2.0",
+	.systemLibrary(name: "CCoglPango", pkgConfig: "cogl-pango",
 	    providers: [
 		.brew(["cogl", "cairo", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libcogl-pango-dev", "libcogl-gles2-dev", "libcogl-path-dev", "libcogl-dev", "libcairo2-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
